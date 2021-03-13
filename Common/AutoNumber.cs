49ea242b54c7b40e52f8b2a3cd2db033425a81ed
Michael Katan <michaelkatan@gmail.com>
2021-03-13 13:27:49 +0200

    Initial Commit

--- Common/AutoNumber.cs
+++ Common/AutoNumber.cs
@@ -1,32 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.ServiceModel;
-using Microsoft.Xrm.Sdk;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
 
 namespace NinjaBank.Common
 {
     public class AutoNumber : IPlugin
     {
+        IOrganizationService service;
         public void Execute(IServiceProvider serviceProvider)
         {
             IPluginExecutionContext context = (IPluginExecutionContext)
                         serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            Entity target = (Entity) context.InputParameters["Target"];
+            Entity target = (Entity) context.InputParameters["Target"]; //new_case (for now)
 
             IOrganizationServiceFactory serviceFactory =
                     (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            service = serviceFactory.CreateOrganizationService(context.UserId);
 
 
-            string entityName = target.LogicalName;
+            string entityName = target.LogicalName; //new_case (for now)
 
-            Entity autoNumber = getAutonumberByEntityName();
-        
+            Entity autoNumber = getAutonumberByEntityName(entityName);
+
+
+            int counter = autoNumber.GetAttributeValue<int>("new_number");
+            counter++;
+
+            string fieldName = autoNumber.GetAttributeValue<string>("new_fieldname");
+            target[fieldName] = counter.ToString();
+
+            autoNumber["new_number"] = counter;
+            service.Update(autoNumber);
+        }
+
+        private Entity getAutonumberByEntityName(string entityName) //new_case
+        {
+
+            Entity autoNumber = null;
+
+            QueryExpression query = new QueryExpression("new_autonumber");  //from new_autonumber
+
+            query.ColumnSet = new ColumnSet("new_number", "new_fieldname"); //select new_number, new_fieldname
+
+            query.Criteria.AddCondition("new_name", ConditionOperator.Equal, entityName); // where new_name == new_case
+
+            EntityCollection result = service.RetrieveMultiple(query);
+
+            if(result.Entities.Count > 0)
+            {
+                autoNumber = result.Entities[0];
+            }
+
+            return autoNumber;
         }
     }
 }
